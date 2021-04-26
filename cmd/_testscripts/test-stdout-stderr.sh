3f34051039832560f50d747456940f1cad91cd3b
Yevgeniy Brikman <brikis98@users.noreply.github.com>
2021-04-26 14:12:03 +0100

    Fix typo

--- cmd/_testscripts/test-stdout-stderr.sh
+++ cmd/_testscripts/test-stdout-stderr.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# This scripts writes some text to stdout and stderr and then exits with an error. This is used to test that git-xargs
+# This script writes some text to stdout and stderr and then exits with an error. This is used to test that git-xargs
 # always logs the stdout and stderr from scripts, even if those scripts exit with an error.
 
 echo 'Hello, from STDOUT'
