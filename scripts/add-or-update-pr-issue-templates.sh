6c945dac2f3f236a2f4e52ccbc0d184eb7fccf36
Rob Morgan <robbym@gmail.com>
2021-11-30 23:07:18 +0800

    update the script for pr issue templates (#60)

--- scripts/add-or-update-pr-issue-templates.sh
+++ scripts/add-or-update-pr-issue-templates.sh
@@ -78,7 +78,7 @@ cat << "EOF" > .github/pull_request_template.md
 <!--
 Have any questions? Check out the contributing docs at https://gruntwork.notion.site/Gruntwork-Coding-Methodology-02fdcd6e4b004e818553684760bf691e,
 or ask in this Pull Request and a Gruntwork core maintainer will be happy to help :)
-Note: Remember to add '[WIP]' to the beginning of the title if this PR is still a work-in-progress.
+Note: Remember to add '[WIP]' to the beginning of the title if this PR is still a work-in-progress. Remove it when it is ready for review!
 -->
 
 ## Description
@@ -96,31 +96,26 @@ Note: Remember to add '[WIP]' to the beginning of the title if this PR is still 
     - Create a stub for documentation including bullet points for how to use the feature, code snippets (including from happy path tests), etc.
 -->
 
-<!-- Important: Did you make any backwards incompatible changes? If yes, then you must write a migration guide! -->
+<!-- Important: Did you make any backward incompatible changes? If yes, then you must write a migration guide! -->
 
 ## TODOs
 
+Please ensure all of these TODOs are completed before asking for a review.
+
 - [ ] Ensure the branch is named correctly with the issue number. e.g: `feature/new-vpc-endpoints-955` or `bug/missing-count-param-434`.
 - [ ] Update the docs.
-- [ ] Keep the changes backwards compatible where possible.
+- [ ] Keep the changes backward compatible where possible.
 - [ ] Run the pre-commit checks successfully.
 - [ ] Run the relevant tests successfully.
-- [ ] Ensure any 3rd party code adheres with our license policy: https://www.notion.so/gruntwork/Gruntwork-licenses-and-open-source-usage-policy-f7dece1f780341c7b69c1763f22b1378
-- [ ] _Maintainers Only._ If necessary, release a new version of this repo.
-- [ ] _Maintainers Only._ If there were backwards incompatible changes, include a migration guide in the release notes.
-- [ ] _Maintainers Only._ Add to the next version of the monthly newsletter (see https://www.notion.so/gruntwork/Monthly-Newsletter-9198cbe7f8914d4abce23dca7b435f43).
+- [ ] Ensure any 3rd party code adheres with our [license policy](https://www.notion.so/gruntwork/Gruntwork-licenses-and-open-source-usage-policy-f7dece1f780341c7b69c1763f22b1378) or delete this line if its not applicable.
 
 
 ## Related Issues
 
 <!--
-  Link to the issue that is fixed by this PR (if there is one)
+  Link to related issues, and issues fixed or partially addressed by this PR.
   e.g. Fixes #1234
-
-  Link to an issue that is partially addressed by this PR (if there are any)
   e.g. Addresses #1234
-
-  Link to related issues (if there are any)
   e.g. Related to #1234
 -->
 EOF
